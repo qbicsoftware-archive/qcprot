933231b5a1c5943014ba09ffabfb02d0cadc082b
Adrian Seyboldt <adrian_seyboldt@web.de>
2015-02-27 15:10:12 +0100

    add program versions in jobscript

--- jobscript.sh
+++ jobscript.sh
@@ -2,9 +2,9 @@
 # properties = {properties}
 
 module load devel/qt/4.8.4
-module load qbic/openms
-module load bio/xtandem
-module load math/R
+module load qbic/openms/1.11.1-2996-g13ffbd7
+module load bio/xtandem/201309011-gnu-4.8
+module load math/R/3.1.0
 
 {exec_job}
 exit 0
