602e66e31139819496d394276de7895a9796c027
Adrian Seyboldt <adrian_seyboldt@web.de>
2015-09-24 13:29:15 +0200

    write logs and stats to log dir

--- jobscript.sh
+++ jobscript.sh
@@ -3,6 +3,8 @@
 #PBS -A qbic
 #PBS -l nodes=1:ppn=10:cfc
 #PBS -l walltime=40:00:00
+#PBS -e ../logs/jobscript.{job.rule.name}.e$PBS_JOBID
+#PBS -o ../logs/jobscript.{job.rule.name}.o$PBS_JOBID
 # properties = {properties}
 
 module load qbic/anaconda
